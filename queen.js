14062eed60ec40f38d1bf0b8fc648683acaf3867
Nadiyaytofc <nadiyaytofc@gmail.com>
2025-08-15 22:36:44 +0530

    Update queen.js

--- queen.js
+++ queen.js
@@ -20,7 +20,7 @@ const express = require("express");
 const app = express();
 const por = require("./session");
 const port = por.PORT;
-const ownerNumber = ["94717775628"];
+const ownerNumber = ["94740952053"];
 if (!app) return;
 // Session directory
 const SESSION_DIR = "./sessions";
@@ -29,7 +29,7 @@ const sess = require("./session");
 async function sessdl() {
   try {
     // Extract the Base64 encoded session data
-    const base64Data = sess.SESSION_ID.split("ANJU-XPRO~")[1];
+    const base64Data = sess.SESSION_ID.split("KING-RAVAN-MD~")[1];
     if (!base64Data) {
       throw new Error("Invalid SESSION_ID format - missing Base64 data");
     }
@@ -91,7 +91,7 @@ async function connectToWA() {
   const getPrefix = () => config.PREFIX;
   const getWelcome = () => config.WELCOME;
   //===========================
-  console.log("🔥 QUEEN ANJU XPRO is starting...");
+  console.log("🔥 KING RAVAN MD is starting...");
   const { state, saveCreds } = await useMultiFileAuthState(
     __dirname + "/sessions/"
   );
@@ -119,9 +119,9 @@ async function connectToWA() {
       console.log("connected to whatsapp ✅");
       // Assuming `config` contains all the settings
       let up = `
-          🚀 **© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚 Connected Successfully!** ✅ 
+          🚀 **© 𝙺𝙸𝙽𝙶 𝚁𝙰𝚅𝙰𝙽 𝙼𝙳 💚 Connected Successfully!** ✅ 
           
-          --- **🎉 Welcome to © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚!** 🎉 
+          --- **🎉 Welcome to © 𝙺𝙸𝙽𝙶 𝚁𝙰𝚅𝙰𝙽 𝙼𝙳 💚!** 🎉 
           ✦» 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${require("./package.json").version}
           ✦» 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
           ✦» 𝙷𝚘𝚜𝚝 : ${os.hostname()}
@@ -150,7 +150,7 @@ async function connectToWA() {
             config.AUTORECORDING ? "Enabled" : "Disabled"
           }
       
-          --- Thank you for using **© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚**. 
+          --- Thank you for using **© 𝙺𝙸𝙽𝙶 𝚁𝙰𝚅𝙰𝙽 𝙼𝙳 💚**. 
           We're here to make your experience enjoyable and seamless. 
           If you need any help or have questions, don't hesitate to ask. 
           
@@ -165,14 +165,14 @@ async function connectToWA() {
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
             newsletterJid: "120363299978149557@newsletter",
-            newsletterName: "© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚",
+            newsletterName: "© 𝙺𝙸𝙽𝙶 𝚁𝙰𝚅𝙰𝙽 𝙼𝙳 💚",
             serverMessageId: 999,
           },
           externalAdReply: {
-            title: "© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚",
-            body: " ©𝐌𝐑 𝐑𝐀𝐒𝐇𝐌𝐈𝐊𝐀 𝐎𝐅𝐂 💚",
+            title: "© 𝙺𝙸𝙽𝙶 𝚁𝙰𝚅𝙰𝙽 𝙼𝙳 💚",
+            body: " ©𝐌𝐑 𝐍𝐀𝐃𝐈𝐋 𝐇𝐀𝐍𝐒𝐀𝐉𝐀💚",
             mediaType: 1,
-            sourceUrl: "https://github.com/Mrrashmika",
+            sourceUrl: "https://github.com/Nadiyatofc/",
             thumbnailUrl:
               "https://raw.githubusercontent.com/RASH-DATA/ANJU-DATA/refs/heads/main/LOGOS/6152181515400889311.jpg", // This should match the image URL provided above
             renderLargerThumbnail: false,
